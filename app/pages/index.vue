bd287a6418d3c85ed5a060f3eeb4a8957544707a
Eric Raslich <rasliche@gmail.com>
2026-09-08 22:11:54 -0400

    courses page and some layouts

---
+++ app/pages/index.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts">
+import type { ButtonProps } from "@nuxt/ui";
+
+const links = ref<ButtonProps[]>([
+  {
+    label: "Courses",
+    to: "/courses",
+    icon: "i-lucide-square-play",
+  },
+//   {
+//     label: "Learn more",
+//     to: "/docs/getting-started/theme/design-system",
+//     color: "neutral",
+//     variant: "subtle",
+//     trailingIcon: "i-lucide-arrow-right",
+//   },
+]);
+</script>
+
+<template>
+  <div>
+    <UPageHero
+      title="A New Learning Management System"
+      description="A Nuxt-based LMS built from the ground up for a quick standup and easy student management."
+      headline="Under active development"
+      :links="links"
+    />
+  </div>
+</template>
